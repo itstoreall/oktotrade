893a7aabb615dfdcfeae2e01f93741df0ce613c0
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-05 19:41:11 +0200

    fixes url

--- js/footer-templating.js
+++ js/footer-templating.js
@@ -8,27 +8,33 @@ refs.footerContainer.insertAdjacentHTML("beforeend", footerMarkup);
 const markupRender = (href) => {
 
    switch(href) {
-      case "/ugol.html":
+      // case "/ugol.html":
+      case "/oktotrade/ugol.html":
          linkHandler('.ugol');
          console.log("1", href);
          break;
-      case "/shlak.html":
+      // case "/shlak.html":
+      case "/oktotrade/shlak.html":
          console.log("2", href);
          linkHandler('.shlak');
          break;
-      case "/shcheben.html":
+      // case "/shcheben.html":
+      case "/oktotrade/shcheben.html":
          console.log("3", href);
          linkHandler('.shcheben');
          break;
-      case "/nefteprodukty-svetlyye.html":
+      // case "/nefteprodukty-svetlyye.html":
+      case "/oktotrade/nefteprodukty-svetlyye.html":
          console.log("4", href);
          linkHandler('.neft-svet');
          break;
-      case "/neftekhimiya.html":
+      // case "/neftekhimiya.html":
+      case "/oktotrade/neftekhimiya.html":
          console.log("5", href);
          linkHandler('.neftekhimiya');
          break;
-      case "/maslo-bazovoye.html":
+      // case "/maslo-bazovoye.html":
+      case "/oktotrade/maslo-bazovoye.html":
          console.log("6", href);
          linkHandler('.maslo-baz');
          break;
