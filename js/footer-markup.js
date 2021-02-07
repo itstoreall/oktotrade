2fa23eeca5774aacf249d204059616d9df5a8726
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-08 01:59:39 +0200

    adds new footer styles

--- js/footer-markup.js
+++ js/footer-markup.js
@@ -1,35 +1,34 @@
-import footerContent from "./footer-items.js"
+import footerContent from "./footer-items.js";
 
 const footerMarkup = footerContent.reduce((content, item) => {
-   return content + `
+  return (
+    content +
+    `
       <div class="footer-content__wrap">
          <!-- Logo -->
-         <a href="./index.html" 
-            class="footer-logo__link"><img 
-            src="./images/svg/logo-okto-trade-white.svg" 
-            alt="окто трейд" 
-            aria-label="окто трейд">
+         <a href="./index.html" class="footer-logo__link">
+            <img src="./images/svg/logo-okto-trade-white.svg" alt="окто трейд" aria-label="окто трейд">
          </a>
             
          <!-- Info -->
-         <nav>
+         <nav class="footer-nav">
             <b class="footer-subtitle">Информация</b>
-            <ul class="footer-nav">
-               <li><a class="footer-nav_link ugol" href="${item.productLink_01}">${item.productName_01}</a></li>
-               <li><a class="footer-nav_link shlak" href="${item.productLink_02}">${item.productName_02}</a></li>
-               <li><a class="footer-nav_link shcheben" href="${item.productLink_03}">${item.productName_03}</a></li>
-               <li><a class="footer-nav_link neft-svet" href="${item.productLink_04}">${item.productName_04}</a></li>
-               <li><a class="footer-nav_link neftekhimiya" href="${item.productLink_05}">${item.productName_05}</a></li>
-               <li><a class="footer-nav_link maslo-baz" href="${item.productLink_06}">${item.productName_06}</a></li>
+            <ul class="footer-nav__list">
+               <li><a class="footer-nav__list-link ugol" href="${item.productLink_01}">${item.productName_01}</a></li>
+               <li><a class="footer-nav__list-link shlak" href="${item.productLink_02}">${item.productName_02}</a></li>
+               <li><a class="footer-nav__list-link shcheben" href="${item.productLink_03}">${item.productName_03}</a></li>
+               <li><a class="footer-nav__list-link neft-svet" href="${item.productLink_04}">${item.productName_04}</a></li>
+               <li><a class="footer-nav__list-link neftekhimiya" href="${item.productLink_05}">${item.productName_05}</a></li>
+               <li><a class="footer-nav__list-link maslo-baz" href="${item.productLink_06}">${item.productName_06}</a></li>
             </ul>
          </nav>
       </div>
-            
+               
       <div class="footer-content__wrap">
          <!-- Address -->
          <address>
             <b class="footer-subtitle">Наши контакты</b>
-            <ul class="footer-address">
+            <ul class="footer-address__list">
                <li>
                   <a class="google-map" href="https://goo.gl/maps/R6bYdpyuvPCBq6NV9">
                      <span>03117 Украина, Киев,</span>
@@ -37,7 +36,7 @@ const footerMarkup = footerContent.reduce((content, item) => {
                   </a>
                </li>
                <li>
-                  <a class="footer-address__link" href="mailto:info@example.com">
+                  <a class="footer-address__list-link" href="mailto:info@example.com">
                      <svg class="address-icon__mail">
                         <use href="./images/svg/sprite.svg#icon_mail"></use>
                      </svg>
@@ -45,7 +44,7 @@ const footerMarkup = footerContent.reduce((content, item) => {
                   </a>
                </li>
                <li>
-                  <a class="footer-address__link" href="tel:+380991111111">
+                  <a class="footer-address__list-link" href="tel:+380991111111">
                      <svg class="address-icon__tel">
                         <use href="./images/svg/sprite.svg#icon_tel"></use>
                      </svg>
@@ -79,7 +78,7 @@ const footerMarkup = footerContent.reduce((content, item) => {
          
       <!-- Copyright -->
       <p class="footer-copyright">ТД ОКТО ТРЕЙД &#169; 2021</p>`
-   }, ``
-);
+  );
+}, ``);
 
 export default footerMarkup;
--- js/footer-templating.js
+++ js/footer-templating.js
@@ -1,58 +1,59 @@
-import refs from './refs.js';
-import footerMarkup from './footer-markup.js';
+import refs from "./refs.js";
+import footerMarkup from "./footer-markup.js";
 
 // Inserts Markup into the DOM
 refs.footerContainer.insertAdjacentHTML("beforeend", footerMarkup);
 
 // Assigns a class to the carrent page link
 const markupRender = (href) => {
-
-   switch(href) {
-      // case "/ugol.html":
-      case "/oktotrade/ugol.html":
-         linkHandler('.ugol');
-         console.log("1", href);
-         break;
-      // case "/shlak.html":
-      case "/oktotrade/shlak.html":
-         console.log("2", href);
-         linkHandler('.shlak');
-         break;
-      // case "/shcheben.html":
-      case "/oktotrade/shcheben.html":
-         console.log("3", href);
-         linkHandler('.shcheben');
-         break;
-      // case "/nefteprodukty-svetlyye.html":
-      case "/oktotrade/nefteprodukty-svetlyye.html":
-         console.log("4", href);
-         linkHandler('.neft-svet');
-         break;
-      // case "/neftekhimiya.html":
-      case "/oktotrade/neftekhimiya.html":
-         console.log("5", href);
-         linkHandler('.neftekhimiya');
-         break;
-      // case "/maslo-bazovoye.html":
-      case "/oktotrade/maslo-bazovoye.html":
-         console.log("6", href);
-         linkHandler('.maslo-baz');
-         break;
-      default:
-         linkHandler('home-page');
-         console.log("0", href);
-         break;
-   };
+  switch (href) {
+    // case "/ugol.html":
+    case "/oktotrade/ugol.html":
+      linkHandler(".ugol");
+      console.log("1", href);
+      break;
+    // case "/shlak.html":
+    case "/oktotrade/shlak.html":
+      console.log("2", href);
+      linkHandler(".shlak");
+      break;
+    // case "/shcheben.html":
+    case "/oktotrade/shcheben.html":
+      console.log("3", href);
+      linkHandler(".shcheben");
+      break;
+    // case "/nefteprodukty-svetlyye.html":
+    case "/oktotrade/nefteprodukty-svetlyye.html":
+      console.log("4", href);
+      linkHandler(".neft-svet");
+      break;
+    // case "/neftekhimiya.html":
+    case "/oktotrade/neftekhimiya.html":
+      console.log("5", href);
+      linkHandler(".neftekhimiya");
+      break;
+    // case "/maslo-bazovoye.html":
+    case "/oktotrade/maslo-bazovoye.html":
+      console.log("6", href);
+      linkHandler(".maslo-baz");
+      break;
+    default:
+      linkHandler("home-page");
+      console.log("0", href);
+      break;
+  }
 };
 
 // Current class toggle
 const linkHandler = (currentClass) => {
-   const links = document.querySelector(`.footer-nav_link`);
-   const currentLink = document.querySelector(`.footer-nav_link${currentClass}`);
+  const links = document.querySelector(`.footer-nav__list-link`);
+  const currentLink = document.querySelector(
+    `.footer-nav__list-link${currentClass}`
+  );
 
-   currentClass !== 'home-page'
-      ? currentLink.classList.add('current')
-      : links.classList.remove('current');
+  currentClass !== "home-page"
+    ? currentLink.classList.add("current")
+    : links.classList.remove("current");
 };
-   
+
 markupRender(window.location.pathname);
